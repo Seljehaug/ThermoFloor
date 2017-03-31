cb9092459b74bd103b0699b50ba227ed2069f4eb
Seljehaug <cs_seljehaug@hotmail.com>
2017-04-01 00:00:59 +0200

    added google maps to articles

--- js/googlemap.js
+++ js/googlemap.js
@@ -1,7 +1,12 @@
-/*function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}*/
+function myMap() {
+	var myCenter = new google.maps.LatLng(60.786062,10.699598);
+	var mapProp= {
+		center: myCenter,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+		// disableDefaultUI: true
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	var marker = new google.maps.Marker({position:myCenter});
+	marker.setMap(map);
+}
